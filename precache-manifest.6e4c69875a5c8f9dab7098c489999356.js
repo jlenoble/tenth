ee031fe6778b96e62b4338a25f7f803b27566f5b
Jason Lenoble <jason.lenoble@gmail.com>
2020-01-04 17:06:06 +0100

    Updates

---
+++ precache-manifest.6e4c69875a5c8f9dab7098c489999356.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c6cee7fdbf3ccfa6d9529871cdd3af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb45587007d0261afe8a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f86e73b45e6c5ffa71da",
+    "url": "/static/js/2.17faf04d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.17faf04d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb45587007d0261afe8a",
+    "url": "/static/js/main.50814509.chunk.js"
+  },
+  {
+    "revision": "c3befb2ad449c3971b7f",
+    "url": "/static/js/runtime-main.522546e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
