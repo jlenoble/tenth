8ee40c440c030e44dc73021c471189c513eb2ede
Jason Lenoble <jason.lenoble@gmail.com>
2020-01-09 19:18:29 +0100

    Vanilla Mui todo app

---
+++ src/sandbox/mui-todo-app/AddItem.tsx
@@ -0,0 +1,45 @@
+import React, { FunctionComponent, ChangeEvent, KeyboardEvent } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+
+export interface AddItemProps {
+  inputValue: string;
+  onInputChange(event: ChangeEvent<HTMLInputElement>): void;
+  onInputKeyPress(event: KeyboardEvent<HTMLInputElement>): void;
+  onButtonClick(): void;
+}
+
+const AddItem: FunctionComponent<AddItemProps> = ({
+  inputValue,
+  onInputChange,
+  onInputKeyPress,
+  onButtonClick
+}) => (
+  <Paper style={{ margin: 16, padding: 16 }}>
+    <Grid container>
+      <Grid xs={10} md={11} item style={{ paddingRight: 16 }}>
+        <TextField
+          placeholder="Add item here"
+          value={inputValue}
+          onChange={onInputChange}
+          onKeyPress={onInputKeyPress}
+          fullWidth
+        />
+      </Grid>
+      <Grid xs={2} md={1} item>
+        <Button
+          fullWidth
+          color="secondary"
+          variant="outlined"
+          onClick={onButtonClick}
+        >
+          Add
+        </Button>
+      </Grid>
+    </Grid>
+  </Paper>
+);
+
+export default AddItem;
---
+++ src/sandbox/mui-todo-app/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InputList from "./InputList";
+
+function App() {
+  return <InputList defaultItems={[]} />;
+}
+
+export default App;
