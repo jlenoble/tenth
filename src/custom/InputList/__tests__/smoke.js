5a9505e10f363ee90c9ef76645c958e66402c2d0
Jason Lenoble <jason.lenoble@gmail.com>
2020-02-24 11:19:25 +0100

    Smoke test for persistent list
    Removed corresponding sandbox app

--- src/custom/InputList/__tests__/smoke.js
+++ src/custom/InputList/__tests__/smoke.js
@@ -2,12 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import InputList from "..";
 
+const todoListKey = "todolist";
+
+const saveItems = items => {
+  localStorage.setItem(todoListKey, JSON.stringify(items));
+};
+
 describe("InputList renders without crashing", () => {
   it("without any attributes", () => {
-    // Default list
     // Items can be added, removed, checked
     // They cannot be edited, persisted, reordered
     const div = document.createElement("div");
     ReactDOM.render(<InputList />, div);
+  });
+
+  it("with persistence", () => {
+    // Items can be added, removed, checked, persisted
+    // They cannot be edited, reordered
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <InputList
+        defaultItems={JSON.parse(localStorage.getItem(todoListKey) || "[]")}
+        onSetItems={saveItems}
+      />,
+      div
+    );
   });
 });
