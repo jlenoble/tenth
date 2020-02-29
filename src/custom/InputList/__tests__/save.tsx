a9c5464483f31398338bb0aa290c81d52150ab19
Jason Lenoble <jason.lenoble@gmail.com>
2020-02-29 13:13:25 +0100

    Test for persistence with itemHooks set

--- src/custom/InputList/__tests__/save.tsx
+++ src/custom/InputList/__tests__/save.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import InputList, { Item, defaultTmpId } from "..";
+import InputList, { useItems, defaultTmpId } from "..";
 import {
   render,
   saveItems,
@@ -114,8 +114,45 @@ describe("Items in InputList can be persisted", () => {
     });
   });
 
-  test.todo("by setting itemHooks");
-  test.todo("by setting defaultItems and itemHooks");
+  it("by setting itemHooks: Parent is in charge of state", async () => {
+    function App() {
+      const itemHooks = useItems(getItems(), setItems);
+
+      return <InputList itemHooks={itemHooks} />;
+    }
+
+    const {
+      add,
+      checkChildren,
+      removeChildren,
+      expectTextContents,
+      expectChecks
+    } = render(<App />);
+    const expectProps = makeExpectProps({ expectTextContents, expectChecks });
+
+    expectProps({
+      texts: ["foo", "bar", "baz"],
+      checks: [false, true, false]
+    });
+
+    await add(["qux", "quux"]);
+    expectProps({
+      texts: ["foo", "bar", "baz", "qux", "quux"],
+      checks: [false, true, false, false, false]
+    });
+
+    checkChildren([0, 1, 4]);
+    expectProps({
+      texts: ["foo", "bar", "baz", "qux", "quux"],
+      checks: [true, false, false, false, true]
+    });
+
+    removeChildren([1, 4, 2]);
+    expectProps({
+      texts: ["foo", "qux"],
+      checks: [true, false]
+    });
+  });
 
   test.todo("by setting localStorageKey");
   test.todo("by setting localStorageKey and itemHooks");
