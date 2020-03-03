87a7b8c2272db822776da3565ad05e7f62c5375d
Jason Lenoble <jason.lenoble@gmail.com>
2020-03-03 18:48:27 +0100

    replaced ts-ignore with type casting

--- src/core/list/List/stateful.tsx
+++ src/core/list/List/stateful.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, PropsWithChildren } from "react";
 import { Item, ItemHooks } from "./item";
 import { useItems, OnSetItems } from "./hooks";
 
@@ -6,7 +6,7 @@ export type Props = {
   itemHooks: ItemHooks;
 };
 
-export type StatefulProps<Props> = Omit<Props, "itemHooks"> & {
+export type StatefulProps<P> = Omit<P, "itemHooks"> & {
   defaultItems?: Item[];
   onSetItems?: OnSetItems;
 };
@@ -18,8 +18,11 @@ export const withItems = <P extends Props>(List: FunctionComponent<P>) => {
     ...other
   }) => {
     const itemHooks = useItems(defaultItems, onSetItems);
-    // @ts-ignore TS2322
-    return <List itemHooks={itemHooks} {...other} />;
+
+    // Wrongly typing "other" as PropsWithChildren<P> to get rid of error TS2322:
+    // "other" is exactly P, +"children", -"itemHooks", but Typescript complains
+    // about potential wrong subtype.
+    return <List {...(other as PropsWithChildren<P>)} itemHooks={itemHooks} />;
   };
 
   return StatefulList;
