6d42768c04d17546f05dd6bd1266612fe53e40c6
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-04 17:47:42 +0200

    Removed unused types

--- src/core/list/AddItem/AddItem.tsx
+++ src/core/list/AddItem/AddItem.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ChangeEvent, KeyboardEvent } from "react";
+import React from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
--- src/core/list/AddItem/StatefulAddItem.tsx
+++ src/core/list/AddItem/StatefulAddItem.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, KeyboardEvent } from "react";
+import React, { FunctionComponent } from "react";
 import { AddItem } from "./AddItem";
 import { ItemHooks } from "../List";
 import { StatefulAddItem as AddItemWrapper } from "../../base";
