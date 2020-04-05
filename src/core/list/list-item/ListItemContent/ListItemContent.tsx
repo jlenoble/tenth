b35bd2c721063c049dd2273c7eba87b319916323
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-05 07:50:26 +0200

    Explicited Item and ItemHooks

--- src/core/list/list-item/ListItemContent/ListItemContent.tsx
+++ src/core/list/list-item/ListItemContent/ListItemContent.tsx
@@ -1,19 +1,25 @@
 import React, { FunctionComponent } from "react";
-import * as Checkbox from "../ListItemCheckbox/ListItemCheckbox";
-import * as DeleteButton from "../ListItemDeleteButton/ListItemDeleteButton";
-import * as EditableText from "../ListItemEditableText/ListItemEditableText";
-import * as Text from "../ListItemText/ListItemText";
+import { ListItemCheckbox } from "../ListItemCheckbox/ListItemCheckbox";
+import { ListItemDeleteButton } from "../ListItemDeleteButton/ListItemDeleteButton";
+import { ListItemText } from "../ListItemText/ListItemText";
 import InlineEdit from "../../InlineEdit";
+import { RequiredKeys } from "../../../../generics";
 
-const ListItemCheckbox = Checkbox.ListItemCheckbox;
-//const ListItemEditableText = EditableText.ListItemEditableText;
-const ListItemText = Text.ListItemText;
-const ListItemDeleteButton = DeleteButton.ListItemDeleteButton;
+export interface Item {
+  id: string;
+  text: string;
+  checked: boolean;
+  edited: boolean;
+}
 
-export type Item = Checkbox.Item & DeleteButton.Item & EditableText.Item;
-export type ItemHooks = Checkbox.ItemHooks &
-  DeleteButton.ItemHooks &
-  EditableText.ItemHooks;
+export interface ItemHooks {
+  checkItem?: (id: string) => void;
+  editItem?: (id: string) => void;
+  removeItem?: (id: string) => void;
+  stopEditing?: () => void;
+  updateItem?: (item: RequiredKeys<Partial<Item>, "id">) => void;
+  updateItemAndStopEditing?: (item: RequiredKeys<Partial<Item>, "id">) => void;
+}
 
 export interface UI {
   checkbox?: boolean;
@@ -30,7 +36,7 @@ export interface Props {
 export const ListItemContent: FunctionComponent<Props> = ({
   item,
   itemHooks = {},
-  ui: { checkbox, deleteButton, editableText } = {}
+  ui: { checkbox, deleteButton, editableText } = {},
 }) => (
   <>
     {checkbox && <ListItemCheckbox item={item} itemHooks={itemHooks} />}
