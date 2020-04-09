241b87b02b18690c320d3272e871663c1cdd476f
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-09 13:18:13 +0200

    add item test

--- src/sandbox/list/__tests__/index.tsx
+++ src/sandbox/list/__tests__/index.tsx
@@ -1,9 +1,9 @@
 import React, { FunctionComponent, useState } from "react";
 
-import { render } from "@testing-library/react";
+import { render, fireEvent, within } from "@testing-library/react";
 import userEvents from "@testing-library/user-event";
 
-import { IconButton } from "@material-ui/core";
+import { Paper, Grid, TextField, Button, IconButton } from "@material-ui/core";
 import { DeleteOutlined } from "@material-ui/icons";
 
 import {
@@ -15,11 +15,16 @@ import {
   ListItemTextProps
 } from "../../../core/base";
 
+import { useInputValue } from "../../../core/states";
+
 const useItems = (initialItems: string[] = []) => {
   const [items, setItems] = useState(initialItems);
 
   return {
     items,
+    add: (value: string) => {
+      setItems(items.concat(value));
+    },
     remove: (index: number) => {
       setItems(items.filter((_, i) => index !== i));
     }
@@ -45,19 +50,47 @@ const ListItem: FunctionComponent<
 const List: FunctionComponent<
   { items?: string[]; listItemProps?: BaseListItemProps } & BaseListProps
 > = ({ items: initialItems = [], listItemProps, ...listProps }) => {
-  const { items, remove } = useItems(initialItems);
+  const { items, add, remove } = useItems(initialItems);
+  const { inputValue, changeInput, keyInput, clearInputAndAdd } = useInputValue(
+    add
+  );
 
   return (
-    <BaseList {...listProps}>
-      {items.map((item, i) => {
-        const hooks = {
-          primary: item,
-          remove: () => remove(i)
-        };
+    <>
+      <Paper style={{ margin: 16, padding: 16 }}>
+        <Grid container>
+          <Grid xs={10} md={11} item style={{ paddingRight: 16 }}>
+            <TextField
+              placeholder="Add item here"
+              value={inputValue}
+              onChange={changeInput}
+              onKeyPress={keyInput}
+              fullWidth
+            />
+          </Grid>
+          <Grid xs={2} md={1} item>
+            <Button
+              fullWidth
+              color="secondary"
+              variant="outlined"
+              onClick={clearInputAndAdd}
+            >
+              Add
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+      <BaseList {...listProps}>
+        {items.map((item, i) => {
+          const hooks = {
+            primary: item,
+            remove: () => remove(i)
+          };
 
-        return <ListItem key={i} hooks={hooks} {...listItemProps} />;
-      })}
-    </BaseList>
+          return <ListItem key={i} hooks={hooks} {...listItemProps} />;
+        })}
+      </BaseList>
+    </>
   );
 };
 
@@ -74,8 +107,49 @@ describe("List", () => {
     ]);
   });
 
+  it("Add", async () => {
+    const { getByRole, getByText, getAllByRole } = render(
+      <List items={["foo"]} />
+    );
+
+    const textbox = getByRole("textbox") as HTMLInputElement;
+    const addButton = getByText(/add/i) as HTMLButtonElement;
+
+    await userEvents.type(textbox, "bar");
+
+    let listitems = getAllByRole("listitem") as HTMLLIElement[];
+    expect(listitems.map((li) => li.textContent)).toEqual(["foo"]);
+
+    userEvents.click(addButton);
+
+    listitems = getAllByRole("listitem") as HTMLLIElement[];
+    expect(listitems.map((li) => li.textContent)).toEqual(["foo", "bar"]);
+
+    await userEvents.type(textbox, "baz");
+
+    listitems = getAllByRole("listitem") as HTMLLIElement[];
+    expect(listitems.map((li) => li.textContent)).toEqual(["foo", "bar"]);
+
+    fireEvent.keyPress(textbox, {
+      key: "Enter",
+      code: 13,
+      charCode: 13,
+      keyCode: 13
+    });
+
+    listitems = getAllByRole("listitem") as HTMLLIElement[];
+    expect(listitems.map((li) => li.textContent)).toEqual([
+      "foo",
+      "bar",
+      "baz"
+    ]);
+  });
+
   it("Remove", () => {
-    const { getAllByRole } = render(<List items={["foo", "bar", "baz"]} />);
+    const { getByRole } = render(<List items={["foo", "bar", "baz"]} />);
+
+    const list = getByRole("list") as HTMLUListElement;
+    const { getAllByRole } = within(list);
 
     let buttons = getAllByRole("button") as HTMLButtonElement[];
     userEvents.click(buttons[0]);
