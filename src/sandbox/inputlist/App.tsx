0924ca2a71b70a69b4521ecb0dc9abbe2705eee8
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-11 08:17:55 +0200

    removed commented lines
    no ui attribute anymore

--- src/sandbox/inputlist/App.tsx
+++ src/sandbox/inputlist/App.tsx
@@ -5,17 +5,7 @@ const List = withLocalStorage(StatefulDnDList);
 const todoListKey = "todolist";
 
 function App() {
-  return (
-    <List
-      // ui={{
-      //   addItem: true,
-      //   checkbox: true,
-      //   deleteButton: true,
-      //   editableText: true
-      // }}
-      localStorageId={todoListKey}
-    />
-  );
+  return <List localStorageId={todoListKey} />;
 }
 
 export default App;
