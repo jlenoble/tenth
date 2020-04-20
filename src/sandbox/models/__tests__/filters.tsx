2cbea351983b07da9cbb6a4cb5855b0a3cba6541
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-20 10:13:25 +0200

    tests for visilbility filters

---
+++ src/sandbox/models/__tests__/filters.tsx
@@ -0,0 +1,116 @@
+import React, { FunctionComponent } from "react";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider, useDispatch } from "react-redux";
+import { render, fireEvent, within } from "@testing-library/react";
+import userEvents from "@testing-library/user-event";
+import { TodoList, combinedReducer } from "../TodoList";
+import { resetTodos, tmpId, watchInputs, watchVisibilityFilter } from "../todo";
+
+const List: FunctionComponent<{ items: string[] }> = ({ items }) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(combinedReducer, applyMiddleware(sagaMiddleware));
+  sagaMiddleware.run(watchVisibilityFilter);
+  sagaMiddleware.run(watchInputs);
+
+  const InnerList: FunctionComponent = () => {
+    const dispatch = useDispatch();
+    dispatch(
+      resetTodos(
+        items.map((item, i) => ({
+          id: tmpId(),
+          title: i ? item : "",
+          completed: !(i % 2)
+        }))
+      )
+    );
+    return <TodoList />;
+  };
+
+  return (
+    <Provider store={store}>
+      <InnerList />
+    </Provider>
+  );
+};
+
+describe("Visibility filters", () => {
+  it("SHOW_ACTIVE", async () => {
+    const { getByRole, getAllByRole } = render(
+      <List items={["foo", "bar", "baz", "qux", "quux", "foobar"]} />
+    );
+
+    const menu = getAllByRole("button")[0] as HTMLButtonElement;
+    userEvents.click(menu);
+
+    const menuItem = within(document.body).getByText("Show active");
+    userEvents.click(menuItem);
+
+    const list = getByRole("list") as HTMLUListElement;
+    const listitems = within(list).getAllByRole("listitem") as HTMLLIElement[];
+
+    expect(listitems.map((li) => li.textContent)).toEqual([
+      "bar",
+      "qux",
+      "foobar"
+    ]);
+  });
+
+  it("SHOW_COMPLETED", async () => {
+    const { getByRole, getAllByRole } = render(
+      <List items={["foo", "bar", "baz", "qux", "quux", "foobar"]} />
+    );
+
+    const menu = getAllByRole("button")[0] as HTMLButtonElement;
+    userEvents.click(menu);
+
+    const menuItem = within(document.body).getByText("Show completed");
+    userEvents.click(menuItem);
+
+    const list = getByRole("list") as HTMLUListElement;
+    const listitems = within(list).getAllByRole("listitem") as HTMLLIElement[];
+
+    expect(listitems.map((li) => li.textContent)).toEqual(["", "baz", "quux"]);
+  });
+
+  it("SHOW_INVALID", async () => {
+    const { getByRole, getAllByRole } = render(
+      <List items={["foo", "bar", "baz", "qux", "quux", "foobar"]} />
+    );
+
+    const menu = getAllByRole("button")[0] as HTMLButtonElement;
+    userEvents.click(menu);
+
+    const menuItem = within(document.body).getByText("Show invalid");
+    userEvents.click(menuItem);
+
+    const list = getByRole("list") as HTMLUListElement;
+    const listitems = within(list).getAllByRole("listitem") as HTMLLIElement[];
+
+    expect(listitems.map((li) => li.textContent)).toEqual([""]);
+  });
+
+  it("SHOW_ALL", async () => {
+    const { getByRole, getAllByRole } = render(
+      <List items={["foo", "bar", "baz", "qux", "quux", "foobar"]} />
+    );
+
+    const menu = getAllByRole("button")[0] as HTMLButtonElement;
+    userEvents.click(menu);
+    userEvents.click(within(document.body).getByText("Show active"));
+    userEvents.click(menu);
+    userEvents.click(within(document.body).getByText("Show all"));
+
+    const list = getByRole("list") as HTMLUListElement;
+    const listitems = within(list).getAllByRole("listitem") as HTMLLIElement[];
+
+    expect(listitems.map((li) => li.textContent)).toEqual([
+      "",
+      "bar",
+      "baz",
+      "qux",
+      "quux",
+      "foobar"
+    ]);
+  });
+});
