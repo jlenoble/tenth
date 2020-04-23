d2058b0c0dea3f66f3cbdebb06a024362c14d5a1
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-23 11:01:38 +0200

    test on titles

--- src/sandbox/models/__tests__/expand.tsx
+++ src/sandbox/models/__tests__/expand.tsx
@@ -5,35 +5,41 @@ import { App, getExpandButtons, getCloseButtons } from "../__testHelpers__";
 
 describe("TodoList", () => {
   it("Expand item", () => {
-    const { getByRole, getAllByRole } = render(
+    const { getByRole, getAllByRole, getAllByText } = render(
       <App items={["foo", "bar", "baz"]} />
     );
 
     expect(Array.from(getAllByRole("list"))).toHaveLength(1);
 
+    // "foo" is the current todo, so shows up also in the main header
+    expect(Array.from(getAllByText("foo"))).toHaveLength(2);
+
     let buttons = getExpandButtons(getByRole("list") as HTMLUListElement);
     userEvents.click(buttons[0]);
 
     expect(Array.from(getAllByRole("list"))).toHaveLength(2);
-    expect(Array.from(getAllByRole("list"))[1]).toBeInTheDocument();
+    expect(Array.from(getAllByText("foo"))).toHaveLength(3);
   });
 
   it("Close item", () => {
-    const { getByRole, getAllByRole } = render(
+    const { getByRole, getAllByRole, getAllByText } = render(
       <App items={["foo", "bar", "baz"]} />
     );
 
     expect(Array.from(getAllByRole("list"))).toHaveLength(1);
+    expect(Array.from(getAllByText("bar"))).toHaveLength(1);
 
     let buttons = getExpandButtons(getByRole("list") as HTMLUListElement);
-    userEvents.click(buttons[0]);
+    userEvents.click(buttons[1]);
 
     expect(Array.from(getAllByRole("list"))).toHaveLength(2);
+    expect(Array.from(getAllByText("bar"))).toHaveLength(2);
 
     buttons = getCloseButtons(document.body);
     expect(buttons).toHaveLength(1);
     userEvents.click(buttons[0]);
 
     expect(Array.from(getAllByRole("list"))).toHaveLength(1);
+    expect(Array.from(getAllByText("bar"))).toHaveLength(1);
   });
 });
