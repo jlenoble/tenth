f8a711272b72f69400f1d0f45b1c0eccba69b799
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-24 09:29:30 +0200

    basic CardManager

--- src/sandbox/App/Main.tsx
+++ src/sandbox/App/Main.tsx
@@ -2,15 +2,27 @@ import React, { FunctionComponent } from "react";
 import { Card, CardHeader, CardContent } from "@material-ui/core";
 import { CloseButton } from "../../core";
 
-export const Main: FunctionComponent = () => {
+type Titles = readonly string[];
+
+const CardManager: FunctionComponent<{ texts: Titles }> = ({ texts }) => {
   return (
-    <Card>
-      <CardHeader
-        action={<CloseButton onClick={() => console.log("click")} />}
-      ></CardHeader>
-      <CardContent>foo</CardContent>
-    </Card>
+    <>
+      {texts.map((text, i) => (
+        <Card key={i}>
+          <CardHeader
+            action={
+              <CloseButton onClick={() => console.log("click " + text)} />
+            }
+          ></CardHeader>
+          <CardContent>{text}</CardContent>
+        </Card>
+      ))}
+    </>
   );
+};
+
+export const Main: FunctionComponent = () => {
+  return <CardManager texts={["foo", "bar", "baz"]} />;
 };
 
 export default Main;
