fddf16133dd3fe152953e80475ae11e1ef5571ad
Jason Lenoble <jason.lenoble@gmail.com>
2020-04-26 12:31:16 +0200

    reorganized generics

--- src/generics/index.ts
+++ src/generics/index.ts
@@ -1,4 +1,2 @@
-export type OmittableKeys<T, K extends keyof T> = Omit<T, K> &
-  Partial<Pick<T, K>>;
-export type RequiredKeys<T, K extends keyof T> = Omit<T, K> &
-  Required<Pick<T, K>>;
+export * from "./keys";
+export * from "./saga";
---
+++ src/generics/keys.ts
@@ -0,0 +1,5 @@
+export type OmittableKeys<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
+
+export type RequiredKeys<T, K extends keyof T> = Omit<T, K> &
+  Required<Pick<T, K>>;
