3b3a5ad980ac3ba3fbed9458feba98cbfe075726
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-05 12:11:44 +0200

    added pagination

--- src/sandbox/apollo-tutorial/resolvers.ts
+++ src/sandbox/apollo-tutorial/resolvers.ts
@@ -1,6 +1,7 @@
 import { GQLUser } from "./__types__/schema";
 import { LaunchAPI } from "./datasources/launch";
 import { UserAPI } from "./datasources/user";
+import { paginateResults } from "./utils";
 
 type Context = {
   dataSources: { launchAPI: LaunchAPI; userAPI: UserAPI<{ user?: GQLUser }> };
@@ -8,8 +9,30 @@ type Context = {
 
 export const resolvers = {
   Query: {
-    launches: (_: any, __: any, { dataSources }: Context) =>
-      dataSources.launchAPI.getAllLaunches(),
+    launches: async (
+      _: any,
+      { pageSize = 20, after }: { pageSize?: number; after?: string },
+      { dataSources }: Context
+    ) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      // we want these in reverse chronological order
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        // if the cursor of the end of the paginated results is the same as the
+        // last item in _all_ results, then there are no more results after this
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false
+      };
+    },
     launch: (_: any, { id }: { id: number }, { dataSources }: Context) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_: any, __: any, { dataSources }: Context) =>
--- src/sandbox/apollo-tutorial/utils.ts
+++ src/sandbox/apollo-tutorial/utils.ts
@@ -35,33 +35,38 @@ export type Store = {
   trips: Trips;
 };
 
-// export const paginateResults = ({
-//   after: cursor,
-//   pageSize = 20,
-//   results,
-//   // can pass in a function to calculate an item's cursor
-//   getCursor = () => null
-// }) => {
-//   if (pageSize < 1) return [];
+export const paginateResults = ({
+  after: cursor,
+  pageSize = 20,
+  results,
+  // can pass in a function to calculate an item's cursor
+  getCursor = () => null
+}: {
+  after?: string;
+  pageSize?: number;
+  results: any[];
+  getCursor?: (item?: any) => string | null;
+}) => {
+  if (pageSize < 1) return [];
 
-//   if (!cursor) return results.slice(0, pageSize);
-//   const cursorIndex = results.findIndex((item) => {
-//     // if an item has a `cursor` on it, use that, otherwise try to generate one
-//     let itemCursor = item.cursor ? item.cursor : getCursor(item);
+  if (!cursor) return results.slice(0, pageSize);
+  const cursorIndex = results.findIndex((item) => {
+    // if an item has a `cursor` on it, use that, otherwise try to generate one
+    let itemCursor = item.cursor ? item.cursor : getCursor(item);
 
-//     // if there's still not a cursor, return false by default
-//     return itemCursor ? cursor === itemCursor : false;
-//   });
+    // if there's still not a cursor, return false by default
+    return itemCursor ? cursor === itemCursor : false;
+  });
 
-//   return cursorIndex >= 0
-//     ? cursorIndex === results.length - 1 // don't let us overflow
-//       ? []
-//       : results.slice(
-//           cursorIndex + 1,
-//           Math.min(results.length, cursorIndex + 1 + pageSize)
-//         )
-//     : results.slice(0, pageSize);
-// };
+  return cursorIndex >= 0
+    ? cursorIndex === results.length - 1 // don't let us overflow
+      ? []
+      : results.slice(
+          cursorIndex + 1,
+          Math.min(results.length, cursorIndex + 1 + pageSize)
+        )
+    : results.slice(0, pageSize);
+};
 
 export const createStore = (): Store => {
   const db = new Sequelize("database", "username", "password", {
