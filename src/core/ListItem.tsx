8b186e0a7d5ca5dd4d22bd8ce08a18a07b70fa0c
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-06 19:11:57 +0200

    fixed missing display name

--- src/core/ListItem.tsx
+++ src/core/ListItem.tsx
@@ -23,69 +23,69 @@ export interface ListItemProps extends ListItemTextProps {
 export type BaseListItemProps = MuiListItemProps;
 export type FullListItemProps = ListItemProps & BaseListItemProps;
 
-export const ListItem: FunctionComponent<FullListItemProps> = memo(
-  ({
-    itemId,
+const ListItem: FunctionComponent<FullListItemProps> = ({
+  itemId,
 
-    checked,
-    checkboxProps,
+  checked,
+  checkboxProps,
 
-    primary,
-    primaryLabel,
-    primaryHelperText,
-    primaryError,
-    primaryEnter,
-    primaryTextFieldProps,
-    listItemTextProps,
+  primary,
+  primaryLabel,
+  primaryHelperText,
+  primaryError,
+  primaryEnter,
+  primaryTextFieldProps,
+  listItemTextProps,
 
-    deleteButtonProps,
-    expandButtonProps,
+  deleteButtonProps,
+  expandButtonProps,
 
-    ...other
-  }) => {
-    return (
-      <MuiListItem {...other}>
-        {checkboxProps && <Checkbox checked={checked} {...checkboxProps} />}
-        <ListItemText
-          primary={primary}
-          primaryLabel={primaryLabel}
-          primaryHelperText={primaryHelperText}
-          primaryError={primaryError}
-          primaryEnter={primaryEnter}
-          primaryTextFieldProps={primaryTextFieldProps}
-          {...listItemTextProps}
-        />
-        {expandButtonProps && <OpenInNewButton {...expandButtonProps} />}
-        {deleteButtonProps && <DeleteButton {...deleteButtonProps} />}
-      </MuiListItem>
-    );
-  },
-  (prevProps, nextProps) => {
-    let eq =
-      prevProps.itemId === nextProps.itemId &&
-      prevProps.primary === nextProps.primary &&
-      prevProps.checked === nextProps.checked;
+  ...other
+}) => {
+  return (
+    <MuiListItem {...other}>
+      {checkboxProps && <Checkbox checked={checked} {...checkboxProps} />}
+      <ListItemText
+        primary={primary}
+        primaryLabel={primaryLabel}
+        primaryHelperText={primaryHelperText}
+        primaryError={primaryError}
+        primaryEnter={primaryEnter}
+        primaryTextFieldProps={primaryTextFieldProps}
+        {...listItemTextProps}
+      />
+      {expandButtonProps && <OpenInNewButton {...expandButtonProps} />}
+      {deleteButtonProps && <DeleteButton {...deleteButtonProps} />}
+    </MuiListItem>
+  );
+};
 
-    const pdp = prevProps.draggableProps;
-    const ndp = nextProps.draggableProps;
+const MemoizedListItem = memo(ListItem, (prevProps, nextProps) => {
+  let eq =
+    prevProps.itemId === nextProps.itemId &&
+    prevProps.primary === nextProps.primary &&
+    prevProps.checked === nextProps.checked;
 
-    if (pdp && ndp) {
-      eq = eq && pdp.draggableId === ndp.draggableId;
-    } else {
-      eq = eq && pdp === ndp;
-    }
+  const pdp = prevProps.draggableProps;
+  const ndp = nextProps.draggableProps;
 
-    const pep = prevProps.expandButtonProps;
-    const nep = nextProps.expandButtonProps;
-
-    if (pep && nep) {
-      eq = eq && pep.className === nep.className;
-    } else {
-      eq = eq && pep === nep;
-    }
-
-    return eq;
+  if (pdp && ndp) {
+    eq = eq && pdp.draggableId === ndp.draggableId;
+  } else {
+    eq = eq && pdp === ndp;
   }
-);
 
-export default ListItem;
+  const pep = prevProps.expandButtonProps;
+  const nep = nextProps.expandButtonProps;
+
+  if (pep && nep) {
+    eq = eq && pep.className === nep.className;
+  } else {
+    eq = eq && pep === nep;
+  }
+
+  return eq;
+});
+
+export { MemoizedListItem as ListItem };
+export default MemoizedListItem;
