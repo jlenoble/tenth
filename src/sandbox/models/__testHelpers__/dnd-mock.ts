51e91ea070e9483d07d6dd0f67b314f1f978b727
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-06 19:00:05 +0200

    removed @ts-ignore lines

--- src/sandbox/models/__testHelpers__/dnd-mock.ts
+++ src/sandbox/models/__testHelpers__/dnd-mock.ts
@@ -1,26 +1,23 @@
 export const mockGetComputedStyle = () => {
-  // @ts-ignore Ignore return type CSSStyleDeclaration (in lib.dom.d.ts)
-  jest.spyOn(window, "getComputedStyle").mockImplementation(() => ({
-    paddingTop: "0px",
-    paddingRight: "0px",
-    paddingBottom: "0px",
-    paddingLeft: "0px",
-    marginTop: "0px",
-    marginRight: "0px",
-    marginBottom: "0px",
-    marginLeft: "0px",
-    borderTopWidth: "0px",
-    borderRightWidth: "0px",
-    borderBottomWidth: "0px",
-    borderLeftWidth: "0px",
-    display: "block",
-  }));
+  jest.spyOn(window, "getComputedStyle").mockImplementation(
+    () =>
+      ({
+        paddingTop: "0px",
+        paddingRight: "0px",
+        paddingBottom: "0px",
+        paddingLeft: "0px",
+        marginTop: "0px",
+        marginRight: "0px",
+        marginBottom: "0px",
+        marginLeft: "0px",
+        borderTopWidth: "0px",
+        borderRightWidth: "0px",
+        borderBottomWidth: "0px",
+        borderLeftWidth: "0px",
+        display: "block",
+      } as CSSStyleDeclaration)
+  );
 };
-
-function toJSON() {
-  // @ts-ignore this may have any type
-  return JSON.stringify(this);
-}
 
 export const mockGetBoundingClientRect = (el: HTMLElement) => {
   jest.spyOn(el, "getBoundingClientRect").mockImplementation(() => ({
@@ -32,6 +29,8 @@ export const mockGetBoundingClientRect = (el: HTMLElement) => {
     height: 0,
     x: 0,
     y: 0,
-    toJSON,
+    toJSON() {
+      return JSON.stringify(this);
+    },
   }));
 };
