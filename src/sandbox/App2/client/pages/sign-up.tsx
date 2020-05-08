37adb9deeb9d87b57925d51a02de4ad5c570626f
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-08 19:57:03 +0200

    factored out login form styles

--- src/sandbox/App2/client/pages/sign-up.tsx
+++ src/sandbox/App2/client/pages/sign-up.tsx
@@ -10,33 +10,15 @@ import {
   Container,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import { makeStyles } from "@material-ui/core/styles";
 import {
   FirstNameTextField,
   LastNameTextField,
   EmailTextField,
   PasswordTextField,
 } from "../components";
+import { makeStyles } from "./sign-in.style";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+const useStyles = makeStyles({ formMarginTopSpacing: 3 });
 
 export default function SignUp() {
   const classes = useStyles();
