54567f9641e970ebdb5e33a3b9ecf6d7c571df69
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-09 07:34:22 +0200

    moved text fields to subdir
    also renamed files

--- src/sandbox/App2/client/components/index.ts
+++ src/sandbox/App2/client/components/index.ts
@@ -1,4 +1,1 @@
-export { EmailTextField } from "./email-textfield";
-export { FirstNameTextField } from "./firstname-textfield";
-export { LastNameTextField } from "./lastname-textfield";
-export { PasswordTextField } from "./password-textfield";
+export * from "./textfields";
---
+++ src/sandbox/App2/client/components/textfields/index.ts
@@ -0,0 +1,4 @@
+export { EmailTextField } from "./email.textfield";
+export { FirstNameTextField } from "./firstname.textfield";
+export { LastNameTextField } from "./lastname.textfield";
+export { PasswordTextField } from "./password.textfield";
