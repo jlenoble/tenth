0138579f7358567f0bdf75e07402042016a54479
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-09 13:30:03 +0200

    imported dashboard template

--- src/sandbox/App2/client/pages/main.tsx
+++ src/sandbox/App2/client/pages/main.tsx
@@ -4,11 +4,13 @@ import { Router } from "@reach/router";
 import { PageContainer } from "../components";
 import { SignIn } from "./sign-in";
 import { SignUp } from "./sign-up";
+import Dashboard from "./dashboard";
 
 export const Main: FunctionComponent = () => {
   return (
     <PageContainer>
       <Router primary={false} component={Fragment}>
+        <Dashboard path="/" />
         <SignIn path="/sign-in" />
         <SignUp path="/sign-up" />
       </Router>
