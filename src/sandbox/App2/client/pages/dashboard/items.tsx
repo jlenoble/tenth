cd008b4e74599d0aa5f98de6af9db5c19efc48c8
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-11 19:14:51 +0200

    add mock list

--- src/sandbox/App2/client/pages/dashboard/items.tsx
+++ src/sandbox/App2/client/pages/dashboard/items.tsx
@@ -3,6 +3,7 @@ import React, { Fragment, FunctionComponent, SyntheticEvent } from "react";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 
+import { List } from "../../../../../core";
 import { Title } from "../../components";
 
 function preventDefault(event: SyntheticEvent): void {
@@ -11,16 +12,39 @@ function preventDefault(event: SyntheticEvent): void {
 
 const useStyles = makeStyles((theme) => ({
   seeMore: {
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(2),
   },
 }));
+
+const items: [number, { primary: string }][] = [
+  [0, { primary: "toto" }],
+  [1, { primary: "totoui" }],
+];
 
 export const Items: FunctionComponent = () => {
   const classes = useStyles();
   return (
     <Fragment>
       <Title>Items</Title>
-
+      <List
+        // droppableId="drop-area"
+        addItemProps={{
+          add: (input = ""): void => {
+            console.log("add", input);
+          },
+        }}
+        listItems={items.map(([id, payload]) => {
+          return {
+            key: id,
+            ...payload,
+            deleteButtonProps: {
+              onClick: (): void => {
+                console.log("delete", id);
+              },
+            },
+          };
+        })}
+      />
       <div className={classes.seeMore}>
         <Link color="primary" href="#" onClick={preventDefault}>
           See more items
