733c19898b83baa689d0d46f0356e0bd8ceeb5ca
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-17 12:44:36 +0200

    auto typescript codegen from gql

--- gulp/types.js
+++ gulp/types.js
@@ -1,27 +1,19 @@
 import gulp from "gulp";
 import path from "path";
-import { generateTypeScriptTypes } from "graphql-schema-typescript";
-import { importSchema } from "graphql-import";
-import { makeExecutableSchema } from "graphql-tools";
-import { GraphQLDateTime } from "graphql-iso-date";
+import execa from "execa";
+import chalk from "chalk";
 import { srcDir } from "./helpers/dirs";
 
 const schemaDir = path.join(srcDir, "sandbox/App2/graphql-schemas");
-const outputDir = path.join(srcDir, "sandbox/App2");
-const options = {};
-
-const startTypeDefGlob = path.join(schemaDir, "index.graphql");
 const allTypeDefGlob = path.join(schemaDir, "**/*.graphql");
-const outputPath = path.join(outputDir, "__types__.ts");
 
 const execTypes = async () => {
   try {
-    const typeDefs = importSchema(startTypeDefGlob);
-    const resolvers = { DateTime: GraphQLDateTime };
-    const schema = makeExecutableSchema({ typeDefs, resolvers });
-    await generateTypeScriptTypes(schema, outputPath, options);
-  } catch (err) {
-    console.error(err);
+    await execa("yarn", ["graphql-codegen"]);
+  } catch (e) {
+    let msg = chalk.yellow(e.stdout);
+    msg += `\n${chalk.red(e.stderr)}`;
+    console.log(msg);
   }
 };
 
