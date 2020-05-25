39d55606f7dd9a9f28880b188ba79bc74b99897a
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-25 16:06:51 +0200

    removed leftover import

--- src/sandbox/App2/managers/index.ts
+++ src/sandbox/App2/managers/index.ts
@@ -2,5 +2,4 @@ export * from "./apollo-client-manager";
 export * from "./apollo-hooks-manager";
 export * from "./db-init-manager";
 export * from "./redux-manager";
-export * from "./redux-hooks-manager";
 export * from "./saga-manager";
