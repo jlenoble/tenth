798a340bbcdde6ca062000206ac8e0d296f077cf
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-26 16:35:24 +0200

    deep add subitems

--- src/sandbox/App2/client/pages/dashboard/main.tsx
+++ src/sandbox/App2/client/pages/dashboard/main.tsx
@@ -4,34 +4,58 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Container, Grid } from "@material-ui/core";
 
 import { ItemId } from "../../../types";
-// import { clientManager } from "../../apollo-client-manager";
 import { RelatedItemsCard } from "./related-items";
 import { mainStyles } from "./dashboard.style";
 
 const useStyles = makeStyles(mainStyles);
 
+const TwoCards: FunctionComponent<{
+  leftCard: { relatedToId: ItemId };
+  rightCard?: { relatedToId: ItemId };
+}> = ({ leftCard, rightCard }) => {
+  const [[leftItemId, rightItemId], setIds] = useState<[ItemId, ItemId]>([
+    leftCard.relatedToId,
+    rightCard?.relatedToId || 0,
+  ]);
+
+  const openRight = (id: ItemId) => {
+    setIds([leftItemId, id]);
+  };
+
+  const closeRight = () => {
+    setIds([leftItemId, 0]);
+  };
+
+  const openSubItem = (id: ItemId) => {
+    setIds([rightItemId, id]);
+  };
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={rightItemId > 0 ? 6 : 12}>
+        <RelatedItemsCard relatedToId={leftItemId} open={openRight} />
+      </Grid>
+      {rightItemId > 0 && (
+        <Grid item xs={12} md={6}>
+          <RelatedItemsCard
+            relatedToId={rightItemId}
+            open={openSubItem}
+            close={closeRight}
+          />
+        </Grid>
+      )}
+    </Grid>
+  );
+};
+
 export const Main: FunctionComponent = () => {
   const classes = useStyles();
-  const [openedItemId, setOpenedItemId] = useState<ItemId>(0);
-  // clientManager.hooks.useItems();
 
   return (
     <main className={classes.content}>
       <div className={classes.appBarSpacer} />
       <Container maxWidth="lg" className={classes.container}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={openedItemId > 0 ? 6 : 12}>
-            <RelatedItemsCard relatedToId={1} open={setOpenedItemId} />
-          </Grid>
-          {openedItemId > 0 && (
-            <Grid item xs={12} md={6}>
-              <RelatedItemsCard
-                relatedToId={openedItemId}
-                close={(): void => setOpenedItemId(0)}
-              />
-            </Grid>
-          )}
-        </Grid>
+        <TwoCards leftCard={{ relatedToId: 1 }} />
       </Container>
     </main>
   );
