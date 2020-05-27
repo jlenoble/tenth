7c125a51d1c1f20c4a548360875a562b47dd6ba6
Jason Lenoble <jason.lenoble@gmail.com>
2020-05-27 18:58:34 +0200

    serverside recursive destroy

--- src/sandbox/App2/graphql-resolvers/mutation.ts
+++ src/sandbox/App2/graphql-resolvers/mutation.ts
@@ -8,13 +8,27 @@ export const mutationResolvers: Required<Omit<
     itemAPI.createItem(item),
   updateItem: (_, item, { dataSources: { itemAPI } }) =>
     itemAPI.updateItem(item),
-  destroyItem: async (
-    _,
-    item,
-    { dataSources: { itemAPI, relationshipAPI } }
-  ) => {
+  destroyItem: async (_, item, context, info) => {
+    const {
+      dataSources: { itemAPI, relationshipAPI },
+    } = context;
+
+    const items = await relationshipAPI.getAllRelatedItems({
+      relatedToId: item.id,
+      relationId: 2,
+    });
+
     const gqlItem = await itemAPI.destroyItem(item);
     await relationshipAPI.destroyRelationshipsForItem(item, gqlItem.userId);
+
+    const destroyItem = mutationResolvers.destroyItem;
+
+    if (typeof destroyItem === "function") {
+      for (const item of items.items) {
+        await destroyItem(_, item, context, info);
+      }
+    }
+
     return gqlItem;
   },
 
