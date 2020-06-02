3bbc81b8d8bc1a113c2dac0a98c910cae01c7a5c
Jason Lenoble <jason.lenoble@gmail.com>
2020-06-02 16:08:36 +0200

    TwoCards to own file

--- src/sandbox/App2/client/pages/dashboard/main.tsx
+++ src/sandbox/App2/client/pages/dashboard/main.tsx
@@ -1,58 +1,11 @@
 import React, { FunctionComponent } from "react";
-
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Grid } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 
-import { Breadcrumbs } from "./breadcrumbs";
-import { RelatedItemsCard } from "./related-items";
 import { mainStyles } from "./dashboard.style";
-import { clientManager } from "../../apollo-client-manager";
+import { TwoCards } from "./two-cards";
 
 const useStyles = makeStyles(mainStyles);
-
-const TwoCards: FunctionComponent = () => {
-  const {
-    currentPath,
-    leftItemId,
-    rightItemId,
-    rightOpened,
-    closeLeft,
-    openRight,
-    closeRight,
-    openRightRight,
-    moveBack,
-  } = clientManager.reduxHooksManager.useTwoCards();
-
-  // Counteract ListItem memoization to not leave UI in an inconsistent state
-  // when currentPath is changed.
-  const viewKey = currentPath.join(":");
-
-  return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Breadcrumbs currentPath={currentPath} moveBack={moveBack} />
-      </Grid>
-      <Grid item xs={12} md={rightOpened ? 6 : 12}>
-        <RelatedItemsCard
-          viewKey={viewKey}
-          relatedToId={leftItemId}
-          open={openRight}
-          close={(currentPath.length > 1 && closeLeft) || undefined}
-        />
-      </Grid>
-      {rightOpened && (
-        <Grid item xs={12} md={6}>
-          <RelatedItemsCard
-            viewKey={viewKey}
-            relatedToId={rightItemId}
-            open={openRightRight}
-            close={closeRight}
-          />
-        </Grid>
-      )}
-    </Grid>
-  );
-};
 
 export const Main: FunctionComponent = () => {
   const classes = useStyles();
