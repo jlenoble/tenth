50011e81237dfa29cd00dc72ee9a894cd2ca8099
Jason Lenoble <jason.lenoble@gmail.com>
2020-06-02 10:47:03 +0200

    Compute keys only once
    because they are statically determinable

--- src/sandbox/App2/sagas/localstorage.ts
+++ src/sandbox/App2/sagas/localstorage.ts
@@ -5,14 +5,17 @@ import * as CurrentPath from "../redux-reducers/current-path/consts";
 import * as NCards from "../redux-reducers/ncards/consts";
 import { ItemId } from "../types";
 
+const currentPathKeys = Object.keys(CurrentPath);
+const nCardsKeys = Object.keys(NCards);
+
 export function* saveCurrentPathSaga(): SagaGenerator {
-  yield take(Object.keys(CurrentPath));
+  yield take(currentPathKeys);
   const currentPath: ItemId[] = yield select(getCurrentPath);
   localStorage.setItem("currentPath", JSON.stringify(currentPath));
 }
 
 export function* saveNCardsSaga(): SagaGenerator {
-  yield take(Object.keys(NCards));
+  yield take(nCardsKeys);
   const nCards: number = yield select(getNCards);
   localStorage.setItem("nCards", JSON.stringify(nCards));
 }
