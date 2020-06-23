e0b64b9d68271198efc92dcd6fbbb25950509900
Jason Lenoble <jason.lenoble@gmail.com>
2020-06-23 13:26:37 +0200

    distinguish First/Last from Relation

--- src/sandbox/App3/types/relationship.ts
+++ src/sandbox/App3/types/relationship.ts
@@ -4,8 +4,8 @@ import { Container } from "./container";
 
 export interface Relationship<
   Relation extends Item = Item,
-  First extends Relation = Relation,
-  Last extends First = First
+  First extends Item = Item,
+  Last extends Item = First
 > extends Container<First, Last> {
   relation: Relation | null | undefined;
   relationId: Item["id"] | -1;
