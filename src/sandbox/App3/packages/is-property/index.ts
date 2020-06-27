4269f15c297be29c4b31327d1975242796889617
Jason Lenoble <jason.lenoble@gmail.com>
2020-06-27 15:53:39 +0200

    added index & isObsoleteProperty alias

---
+++ src/sandbox/App3/packages/is-property/index.ts
@@ -0,0 +1,5 @@
+export * from "./is-object-property";
+export * from "./is-function-property";
+export * from "./is-object-constructor-property";
+
+export { isObsoletePrimitiveFunctionProperty as isObsoleteProperty } from "./is-function-property";
