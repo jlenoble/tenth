dfed6f0a1ddf9990ac0c05dd29606924ed6eb9da
Jason Lenoble <jason.lenoble@gmail.com>
2020-06-28 16:10:28 +0200

    extracted loops on names

--- src/sandbox/App3/packages/testsuite/it.ts
+++ src/sandbox/App3/packages/testsuite/it.ts
@@ -14,11 +14,36 @@ export const execIt = ({
   const test: Test =
     (set.has(name) && testSuite[name]) ||
     (({ title }: DefaultTestOptions) => {
-      it.todo(`> Write a test for ${title}`);
+      it.todo(`Write a test for "${title}"`);
     });
 
   test({
     title,
     it: (fn) => it(title, fn),
   });
+};
+
+export const execIts = ({
+  testSuite,
+  names,
+  title,
+}: {
+  testSuite: TestSuiteArg;
+  names: string[];
+  title: (name: string) => string;
+}): void => {
+  const set = new Set(Object.getOwnPropertyNames(testSuite));
+
+  for (const name of names) {
+    execIt({ testSuite, name, title: title(name), set });
+    set.delete(name);
+  }
+
+  for (const name of set) {
+    it.todo(
+      `Either add/implement "${title(
+        name
+      )}" or remove/spread "${name}" from/into test suite `
+    );
+  }
 };
--- src/sandbox/App3/packages/testsuite/testsuite.ts
+++ src/sandbox/App3/packages/testsuite/testsuite.ts
@@ -1,12 +1,6 @@
 import { getKeys } from "../get-keys";
-import {
-  TestSuiteArg,
-  DefaultTestOptions,
-  AnyObject,
-  AnyClass,
-  AnyArgs,
-} from "./types";
-import { execIt } from "./it";
+import { TestSuiteArg, AnyObject, AnyClass, AnyArgs } from "./types";
+import { execIts } from "./it";
 
 export const makeTestSuite = <Ctor extends AnyClass>(
   Class: Ctor,
@@ -21,35 +15,23 @@ export const makeTestSuite = <Ctor extends AnyClass>(
           lastConstructor: Function,
           excludeKeys: ["prototype"],
         });
-        const set = new Set(Object.getOwnPropertyNames(TestSuite));
 
-        for (const name of names) {
-          const title = `${Class.name}.${name}`;
-          execIt({ testSuite: TestSuite, name, title, set });
-          set.delete(name);
-        }
-
-        for (const name of set) {
-          it.todo(`Remove Testsuite.${name} or add ${Class.name}.${name}`);
-        }
+        execIts({
+          testSuite: TestSuite,
+          names,
+          title: (name: string) => `${Class.name}.${name}`,
+        });
       }
 
       {
         const obj = new Class(...initArgs);
         const names = getKeys(obj, "properties");
-        const set = new Set(Object.getOwnPropertyNames(testSuite));
 
-        for (const name of names) {
-          const title = `${Class.name}.prototype.${name}`;
-          execIt({ testSuite, name, title, set });
-          set.delete(name);
-        }
-
-        for (const name of set) {
-          it.todo(
-            `Remove testsuite.${name} or add ${Class.name}.prototype.${name}`
-          );
-        }
+        execIts({
+          testSuite,
+          names,
+          title: (name: string) => `${Class.name}.prototype.${name}`,
+        });
       }
     });
   };
