d3a174ff7501df0c6393847b9b808f9b3c4f11e8
Jason Lenoble <jason.lenoble@gmail.com>
2020-06-30 12:01:55 +0200

    implemented doubly linked list

---
+++ src/sandbox/App3/packages/data-structures/doubly-linked-list/__tests__/doubly-linked-list.ts
@@ -0,0 +1,162 @@
+import { makeTestSuite, TestSuite } from "../../../testsuite";
+import { DoublyLinkedList } from "../doubly-linked-list";
+import { DoublyLinkedListConstructor } from "../types";
+
+const tests = (
+  DoublyLinkedList: DoublyLinkedListConstructor<number>
+): TestSuite => ({
+  size(): void {
+    it("reading size", () => {
+      const l = new DoublyLinkedList();
+      l.append(1).append(2).append(3);
+
+      expect(l.size).toBe(3);
+    });
+  },
+
+  head(): void {
+    it("reading head", () => {
+      const l = new DoublyLinkedList();
+      l.append(1).append(2).append(3);
+
+      expect(l.head).toBe(1);
+    });
+  },
+
+  tail(): void {
+    it("reading head", () => {
+      const l = new DoublyLinkedList();
+      l.append(1).append(2).append(3);
+
+      expect(l.tail).toBe(3);
+    });
+  },
+
+  append(): void {
+    it("appending", () => {
+      const l = new DoublyLinkedList();
+
+      l.append(1);
+
+      expect(l.size).toBe(1);
+      expect(Array.from(l)).toEqual([1]);
+
+      l.append(2).append(3);
+
+      expect(l.size).toBe(3);
+      expect(Array.from(l)).toEqual([1, 2, 3]);
+
+      l.append(2).append(1).append(0);
+
+      expect(l.size).toBe(6);
+      expect(Array.from(l)).toEqual([1, 2, 3, 2, 1, 0]);
+    });
+  },
+
+  prepend(): void {
+    it("prepending", () => {
+      const l = new DoublyLinkedList();
+
+      l.prepend(1);
+
+      expect(l.size).toBe(1);
+      expect(Array.from(l)).toEqual([1]);
+
+      l.prepend(2).prepend(3);
+      expect(Array.from(l)).toEqual([3, 2, 1]);
+
+      expect(l.size).toBe(3);
+
+      l.prepend(2).prepend(1).prepend(0);
+      expect(Array.from(l)).toEqual([0, 1, 2, 3, 2, 1]);
+
+      expect(l.size).toBe(6);
+    });
+  },
+
+  deleteHead(): void {
+    it("deleting head", () => {
+      const l = new DoublyLinkedList();
+      l.append(1).append(2).append(3);
+
+      expect(l.size).toBe(3);
+      expect(Array.from(l)).toEqual([1, 2, 3]);
+
+      const n1 = l.deleteHead();
+
+      expect(l.size).toBe(2);
+      expect(Array.from(l)).toEqual([2, 3]);
+      expect(n1?.value).toBe(1);
+
+      const n2 = l.deleteHead();
+      const n3 = l.deleteHead();
+
+      expect(l.size).toBe(0);
+      expect(Array.from(l)).toEqual([]);
+      expect(n2?.value).toBe(2);
+      expect(n3?.value).toBe(3);
+
+      const n4 = l.deleteHead();
+
+      expect(l.size).toBe(0);
+      expect(Array.from(l)).toEqual([]);
+      expect(n4).toBeNull();
+    });
+  },
+
+  deleteTail(): void {
+    it("deleting tail", () => {
+      const l = new DoublyLinkedList();
+      l.append(1).append(2).append(3);
+
+      expect(l.size).toBe(3);
+      expect(Array.from(l)).toEqual([1, 2, 3]);
+
+      const n1 = l.deleteTail();
+
+      expect(l.size).toBe(2);
+      expect(Array.from(l)).toEqual([1, 2]);
+      expect(n1?.value).toBe(3);
+
+      const n2 = l.deleteTail();
+      const n3 = l.deleteTail();
+
+      expect(l.size).toBe(0);
+      expect(Array.from(l)).toEqual([]);
+      expect(n2?.value).toBe(2);
+      expect(n3?.value).toBe(1);
+
+      const n4 = l.deleteTail();
+
+      expect(l.size).toBe(0);
+      expect(Array.from(l)).toEqual([]);
+      expect(n4).toBeNull();
+    });
+  },
+});
+
+const staticTests = (
+  DoublyLinkedList: DoublyLinkedListConstructor<number>
+): TestSuite => ({
+  length({ it }) {
+    it(() => {
+      expect(DoublyLinkedList.length).toBe(0);
+    });
+  },
+
+  name({ it }) {
+    it(() => {
+      if (DoublyLinkedList.name.includes("Sized")) {
+        expect(DoublyLinkedList.name).toBe("SizedDoublyLinkedList");
+      } else {
+        expect(DoublyLinkedList.name).toBe("DoublyLinkedList");
+      }
+    });
+  },
+});
+
+makeTestSuite(
+  DoublyLinkedList,
+  tests(DoublyLinkedList),
+  staticTests(DoublyLinkedList)
+);
---
+++ src/sandbox/App3/packages/data-structures/doubly-linked-list/doubly-linked-list.ts
@@ -0,0 +1,126 @@
+import { DoublyLinkedList as DoublyLinkedListInterface } from "./types";
+import { DoublyLinkedListNode } from "./doubly-linked-list-node";
+
+export class DoublyLinkedList<T> implements DoublyLinkedListInterface<T> {
+  #head: DoublyLinkedListNode<T> | null;
+  #tail: DoublyLinkedListNode<T> | null;
+
+  get head(): T | null {
+    if (this.#head === null) {
+      return null;
+    }
+    return this.#head.value;
+  }
+
+  get tail(): T | null {
+    if (this.#tail === null) {
+      return null;
+    }
+    return this.#tail.value;
+  }
+
+  get size(): number {
+    // Inefficient. If called often, use SizedDoublyLinkedList instead
+    let n = 0;
+    let node = this.#head;
+
+    while (node !== null) {
+      node = node.next;
+      n++;
+    }
+
+    return n++;
+  }
+
+  *[Symbol.iterator](): IterableIterator<T> {
+    let node = this.#head;
+
+    while (node !== null) {
+      yield node.value;
+      node = node.next;
+    }
+  }
+
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+  }
+
+  append(value: T): this {
+    const node = new DoublyLinkedListNode(value, this.#tail);
+
+    if (this.#tail === null) {
+      this.#head = node;
+      this.#tail = node;
+      return this;
+    }
+
+    this.#tail.next = node;
+    this.#tail = node;
+
+    return this;
+  }
+
+  prepend(value: T): this {
+    const node = new DoublyLinkedListNode(value, null, this.#head);
+
+    if (this.#head === null) {
+      this.#head = node;
+      this.#tail = node;
+      return this;
+    }
+
+    this.#head.previous = node;
+    this.#head = node;
+
+    return this;
+  }
+
+  deleteHead(): DoublyLinkedListNode<T> | null {
+    if (this.#head === null) {
+      return null;
+    }
+
+    const head = this.#head;
+
+    if (this.#tail === head) {
+      this.#head = null;
+      this.#tail = null;
+      return head;
+    }
+
+    const next = head.next;
+
+    if (next !== null) {
+      next.previous = null;
+    }
+
+    this.#head = next;
+
+    return head;
+  }
+
+  deleteTail(): DoublyLinkedListNode<T> | null {
+    if (this.#tail === null) {
+      return null;
+    }
+
+    const tail = this.#tail;
+
+    if (this.#head === tail) {
+      this.#head = null;
+      this.#tail = null;
+      return tail;
+    }
+
+    const previous = tail.previous;
+
+    if (previous !== null) {
+      previous.next = null;
+    }
+
+    this.#tail = previous;
+
+    return tail;
+  }
+}
