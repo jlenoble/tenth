5543f567fc1d0b2e2f990d82059dc615d36f2429
Jason Lenoble <jason.lenoble@gmail.com>
2020-07-07 14:17:48 +0200

    added bftNodeIterateWithDepth test

--- src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/binary-tree-node.ts
+++ src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/binary-tree-node.ts
@@ -441,6 +441,105 @@ export const tests = <T>(
     });
   },
 
+  bftNodeIterateWithDepth({ it }): void {
+    it(() => {
+      const o1 = { a: 1 };
+      const o2 = { a: 2 };
+      const o3 = { a: 3 };
+      const o4 = { a: 4 };
+      const o5 = { a: 5 };
+      const o6 = { a: 6 };
+      const o7 = { a: 7 };
+      const o8 = { a: 8 };
+
+      const comparator = new Comparator<typeof o1>((o1, o2) =>
+        o1.a === o2.a ? 0 : o1.a < o2.a ? -1 : 1
+      );
+
+      const node1 = new Structure(o1, comparator);
+      const node2 = new Structure(o2, comparator);
+      const node3 = new Structure(o3, comparator);
+      const node4 = new Structure(o4, comparator);
+      const node5 = new Structure(o5, comparator);
+      const node6 = new Structure(o6, comparator);
+      const node7 = new Structure(o7, comparator);
+      const node8 = new Structure(o8, comparator);
+
+      node3.left = node1;
+      node1.right = node2;
+      node3.right = node4;
+      node4.right = node6;
+      node6.left = node5;
+      node6.right = node8;
+      node8.left = node7;
+
+      const d1 = { node: o1, depth: 1 };
+      const d2 = { node: o2, depth: 2 };
+      const d3 = { node: o3, depth: 0 };
+      const d4 = { node: o4, depth: 1 };
+      const d5 = { node: o5, depth: 3 };
+      const d6 = { node: o6, depth: 2 };
+      const d7 = { node: o7, depth: 4 };
+      const d8 = { node: o8, depth: 3 };
+
+      expect(
+        Array.from(node3.bftNodeIterateWithDepth()).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d3, d1, d4, d2, d6, d5, d8, d7]);
+
+      expect(
+        Array.from(node1.bftNodeIterateWithDepth(1)).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d1, d2]);
+
+      expect(
+        Array.from(node2.bftNodeIterateWithDepth(2)).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d2]);
+
+      expect(
+        Array.from(node4.bftNodeIterateWithDepth(1)).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d4, d6, d5, d8, d7]);
+
+      expect(
+        Array.from(node6.bftNodeIterateWithDepth(2)).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d6, d5, d8, d7]);
+
+      expect(
+        Array.from(node5.bftNodeIterateWithDepth(3)).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d5]);
+
+      expect(
+        Array.from(node8.bftNodeIterateWithDepth(3)).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d8, d7]);
+
+      expect(
+        Array.from(node7.bftNodeIterateWithDepth(4)).map(({ node, depth }) => ({
+          node: node.value,
+          depth,
+        }))
+      ).toEqual([d7]);
+    });
+  },
+
   right: false, // see left
   parent: false, // see left
   root: false, // see left
