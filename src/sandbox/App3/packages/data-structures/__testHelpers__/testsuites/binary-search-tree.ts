5b7f44c7bb53fc5ba6ee77a5fb12602eba16b887
Jason Lenoble <jason.lenoble@gmail.com>
2020-07-09 16:19:05 +0200

    added rdftNodeIterate/rbftNodeIterate

--- src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/binary-search-tree.ts
+++ src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/binary-search-tree.ts
@@ -194,6 +194,16 @@ export const tests = <T>(
       });
     },
 
+    rdftNodeIterate(): void {
+      it("Reverse depth first traversal", () => {
+        const tree = new Structure(initArgs);
+
+        expect(
+          Array.from(tree.rdftNodeIterate()).map((node) => node.value)
+        ).toEqual(sortedArgs.slice().reverse());
+      });
+    },
+
     bftNodeIterate(): void {
       it("Breadth first traversal", () => {
         let tree = new Structure(sortedArgs);
@@ -265,6 +275,27 @@ export const tests = <T>(
         expect(
           Array.from(tree.bftNodeIterate()).map((node) => node.value)
         ).toEqual([args[2], args[1], args[3], args[0], args[5], args[4]]);
+      });
+    },
+
+    rbftNodeIterate(): void {
+      it("Reverse breadth first traversal", () => {
+        const tree = new Structure();
+        const args = sortedArgs.slice(0, 6);
+
+        tree.insert(args[2]);
+        tree.insert(args[3]);
+        tree.insert(args[1]);
+        tree.insert(args[5]);
+        tree.insert(args[0]);
+        tree.insert(args[4]);
+
+        expect(Array.from(tree)).toEqual(args);
+        expect(
+          Array.from(tree.rbftNodeIterate()).map((node) => node.value)
+        ).toEqual(
+          [args[2], args[1], args[3], args[0], args[5], args[4]].reverse()
+        );
       });
     },
 
