07949c55640605b0279a604f7215be92f748237e
Jason Lenoble <jason.lenoble@gmail.com>
2020-07-10 10:29:36 +0200

    test for deleteEdge

--- src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/graph-vertex.ts
+++ src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/graph-vertex.ts
@@ -29,8 +29,8 @@ export const tests = <T>(
           return new Structure(a);
         });
 
-        const edges = vertices.map((v1) => {
-          return vertices.map((v2) => {
+        vertices.forEach((v1) => {
+          vertices.forEach((v2) => {
             const edge = new GraphEdge<T>(v1, v2);
             v1.addEdge(edge);
             v2.addEdge(edge);
@@ -39,6 +39,38 @@ export const tests = <T>(
 
         vertices.forEach((v) => {
           expect(v.degree).toBe(2 * sortedArgs.length - 1);
+        });
+      });
+    },
+
+    deleteEdge(): void {
+      it("deleting edges", () => {
+        const vertices = initArgs.map((a) => {
+          return new Structure(a);
+        });
+
+        const edges = vertices.map((v1) => {
+          return vertices.map((v2) => {
+            const edge = new GraphEdge<T>(v1, v2);
+            v1.addEdge(edge);
+            v2.addEdge(edge);
+            return edge;
+          });
+        });
+
+        vertices.forEach((v) => {
+          expect(v.degree).toBe(2 * sortedArgs.length - 1);
+        });
+
+        vertices[0].deleteEdge(edges[0][0]);
+        vertices[2].deleteEdge(edges[2][1]);
+
+        vertices.forEach((v, i) => {
+          if (i === 0 || i === 2) {
+            expect(v.degree).toBe(2 * sortedArgs.length - 2);
+          } else {
+            expect(v.degree).toBe(2 * sortedArgs.length - 1);
+          }
         });
       });
     },
