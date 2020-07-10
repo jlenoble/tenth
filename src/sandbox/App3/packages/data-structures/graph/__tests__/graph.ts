49e12eeb843571195cfafc103a339b3619cf7093
Jason Lenoble <jason.lenoble@gmail.com>
2020-07-10 15:59:03 +0200

    testsuite for digraph

--- src/sandbox/App3/packages/data-structures/graph/__tests__/graph.ts
+++ src/sandbox/App3/packages/data-structures/graph/__tests__/graph.ts
@@ -5,6 +5,12 @@ import { tests } from "../../__testHelpers__/testsuites/graph";
 
 const initArgs = [1, 10, 12, 5, 12, 2, 12, 33, 21];
 
+class DirectedGraph extends Graph<number> {
+  constructor() {
+    super(true);
+  }
+}
+
 makeTestSuite(
   Graph,
   tests(Graph, initArgs),
@@ -12,5 +18,15 @@ makeTestSuite(
     length: 0,
     name: "Graph",
     Structure: Graph,
+  })
+);
+
+makeTestSuite(
+  DirectedGraph,
+  tests(DirectedGraph, initArgs),
+  staticTests({
+    length: 0,
+    name: "DirectedGraph",
+    Structure: DirectedGraph,
   })
 );
