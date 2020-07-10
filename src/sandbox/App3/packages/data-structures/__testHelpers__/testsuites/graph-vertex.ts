c9db896e193f16dc855f63b3c3654c0a02aa0b26
Jason Lenoble <jason.lenoble@gmail.com>
2020-07-10 11:35:32 +0200

    test for has Edge

--- src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/graph-vertex.ts
+++ src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/graph-vertex.ts
@@ -11,6 +11,14 @@ export const tests = <T>(
 ): TestSuite => {
   initArgs = fillInitArgs(initArgs);
   const sortedArgs = Array.from(new Set(initArgs)).sort(defaultCompare);
+  const duplicates: Map<T, Set<number>> = new Map(
+    sortedArgs.map((arg) => [arg, new Set()])
+  );
+
+  initArgs.forEach((arg, i) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    duplicates.get(arg)!.add(i);
+  });
 
   return {
     value(): void {
@@ -98,6 +106,43 @@ export const tests = <T>(
           } else {
             expect(v.degree).toBe(2 * sortedArgs.length - 1);
           }
+        });
+      });
+    },
+
+    hasEdge(): void {
+      it("Testing edges", () => {
+        const vertices = initArgs.map((a) => {
+          return new Structure(a);
+        });
+
+        const edges = vertices.map((v1) => {
+          return vertices.map((v2) => {
+            const edge = new GraphEdge<T>(v1, v2);
+            v1.addEdge(edge);
+            v2.addEdge(edge);
+            return edge;
+          });
+        });
+
+        vertices[0].deleteEdge(edges[0][0]);
+        vertices[1].deleteEdge(edges[1][2]);
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const s0 = duplicates.get(initArgs[0])!;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const s1 = duplicates.get(initArgs[1])!;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const s2 = duplicates.get(initArgs[2])!;
+
+        vertices.forEach((v, i) => {
+          vertices.forEach((_, j) => {
+            if ((s0.has(i) && s0.has(j)) || (s1.has(i) && s2.has(j))) {
+              expect(v.hasEdge(edges[i][j])).toBe(false);
+            } else {
+              expect(v.hasEdge(edges[i][j])).toBe(true);
+            }
+          });
         });
       });
     },
