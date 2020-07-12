ec7fe6dae64f89ada0875c0b56f31fd7c621489d
Jason Lenoble <jason.lenoble@gmail.com>
2020-07-12 10:36:17 +0200

    implemented set, size, isEmpty

--- src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/mapmap.ts
+++ src/sandbox/App3/packages/data-structures/__testHelpers__/testsuites/mapmap.ts
@@ -3,13 +3,34 @@ import { MapMapConstructor } from "../../mapmap/types";
 import { defaultCompare } from "../../../comparator";
 import { tests as dataStructureTests } from "./data-structure";
 
-export const tests = <T>(
-  Structure: MapMapConstructor<T>,
-  initArgs: T[]
+export const tests = (
+  Structure: MapMapConstructor<number, number, number>,
+  initArgs: number[]
 ): TestSuite => {
   const sortedArgs = Array.from(new Set(initArgs)).sort(defaultCompare);
 
   return {
     ...dataStructureTests(Structure, sortedArgs),
+
+    set(): void {
+      it("Setting elements", () => {
+        const m = new Structure();
+
+        expect(m.isEmpty()).toBe(true);
+        expect(m.size).toBe(0);
+
+        for (const a of initArgs) {
+          for (const b of initArgs) {
+            m.set(a, b, 1);
+          }
+        }
+
+        expect(m.isEmpty()).toBe(false);
+        expect(m.size).toBe(sortedArgs.length ** 2);
+      });
+    },
+
+    size: false, // see "set"
+    isEmpty: false, // see "set"
   };
 };
--- src/sandbox/App3/packages/data-structures/mapmap/mapmap.ts
+++ src/sandbox/App3/packages/data-structures/mapmap/mapmap.ts
@@ -1,3 +1,32 @@
 import { MapMap as MapMapInterface } from "./types";
 
-export class MapMap<U, V, T> implements MapMapInterface<U, V, T> {}
+export class MapMap<A, B, T> implements MapMapInterface<A, B, T> {
+  #rows: Map<A, Map<B, T>> = new Map();
+  #size = 0;
+
+  get size(): number {
+    return this.#size;
+  }
+
+  isEmpty(): boolean {
+    return this.#size === 0;
+  }
+
+  set(a: A, b: B, value: T): void {
+    let row: Map<B, T>;
+
+    if (this.#rows.has(a)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      row = this.#rows.get(a)!;
+    } else {
+      row = new Map();
+      this.#rows.set(a, row);
+    }
+
+    if (!row.has(b)) {
+      this.#size++;
+    }
+
+    row.set(b, value);
+  }
+}
