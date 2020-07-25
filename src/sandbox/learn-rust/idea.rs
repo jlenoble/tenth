eac64e3cc55957e6eecfbf89dbd2baaf94d34f3b
Jason Lenoble <jason.lenoble@gmail.com>
2020-07-25 20:30:00 +0200

    impl From<&str>

--- src/sandbox/learn-rust/idea.rs
+++ src/sandbox/learn-rust/idea.rs
@@ -7,6 +7,12 @@ impl Idea {
   }
 }
 
+impl From<&str> for Idea {
+  fn from(idea: &str) -> Self {
+    Idea(String::from(idea))
+  }
+}
+
 #[test]
 fn new_idea() {
   assert_eq!(Idea::new(), Idea(String::from("")));
@@ -17,4 +23,10 @@ fn default_idea() {
   assert_eq!(Idea::default(), Idea::new());
   let idea: Idea = Default::default();
   assert_eq!(idea, Idea(String::from("")));
+}
+
+#[test]
+fn from_str() {
+  let idea = Idea("idea".to_string());
+  assert_eq!(idea, Idea::from("idea"));
 }
